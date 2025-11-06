b2500d0318c26806c5d43758bec2fefbd1c3ff66
sanderson.ruben@ah.nl <sanderson.ruben@ah.nl>
2025-11-06 13:17:55 +0000

    three-persons
    Groetjes

--- exercises/exercise_1/opdracht_1.py
+++ exercises/exercise_1/opdracht_1.py
@@ -1,2 +1,10 @@
 def likes(team: list) -> str:
-    return True
+    if not team:
+        return "no one likes this"
+
+    if len(team) == 1:
+        return f"{team[0]} likes this"
+
+    # Als er meer dan één naam is, maak een mooie string:
+    names = ", ".join(team[:-1]) + f" and {team[-1]}"
+    return f"{names} like this"
