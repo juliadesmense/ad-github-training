18c7a227254b69e04e105c6393d0a807867269b0
julia.desmense@ah.nl <julia.desmense@ah.nl>
2025-11-06 13:14:21 +0000

    ex 1

--- exercises/exercise_1/opdracht_1.py
+++ exercises/exercise_1/opdracht_1.py
@@ -1,2 +1,7 @@
 def likes(team: list) -> str:
-    return True
+    if(len(team)==0):
+        return "no one likes this"
+    if(len(team)==1):
+        return team[0] + " likes this"
+    if(len(team)>1):
+        return team[0]+" en "+team[1] +  " like this"
